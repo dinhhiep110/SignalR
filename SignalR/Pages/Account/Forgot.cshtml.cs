440d582b186307abb7ccb83ba5e1b3887d6d8396
Duy Hiep <dinhhiep110@gmail.com>
2022-11-07 08:05:14 +0700

    Add send email

--- SignalR/Pages/Account/Forgot.cshtml.cs
+++ SignalR/Pages/Account/Forgot.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SignalR.Models;
+using System.Net.Mail;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace SignalR.Pages.Account
 {
@@ -31,6 +33,27 @@ namespace SignalR.Pages.Account
                 {
                     ViewData["errMsg"] = "Email is not existed";
                     return Page();
+                }
+                var body = $@"<p>Thank you, we have received your request for your password!</p>
+                    Please don't leak this email to everyone.  Thanks!<br/>";
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "example@gmail.com",  // replace with valid value
+                        Password = "password"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    var message = new MailMessage();
+                    message.To.Add(Email);
+                    message.Subject = "Fourth Coffee - New Order";
+                    message.Body = body;
+                    message.IsBodyHtml = true;
+                    message.From = new MailAddress("shop@cart.com");
+                    await smtp.SendMailAsync(message);
                 }
             }
             return Redirect("/Account/Login");
